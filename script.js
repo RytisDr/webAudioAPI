6cab77ba05e882f29446ab5d12730eadb444182d
RytisDr <r.drazdauskas@gmail.com>
2018-11-21 22:26:44 +0100

    Adds some comments, fixes errors (prevents autoplay), adds different colors to different audio data sizes, looks glitchy, that is nice.

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
-/* https://medium.com/@ginalee1114/how-to-music-visualizer-web-audio-api-aa007f4ea525?fbclid=IwAR02MMQepNLRl8RZ59VyUOw_LJwNlf2344a-PtMwwtqG0rH_O7QW7SRAoMg */
-/* Gina Lee */
+/* https://medium.com/@ginalee1114/how-to-music-visualizer-web-audio-api-aa007f4ea525?fbclid=IwAR02MMQepNLRl8RZ59VyUOw_LJwNlf2344a-PtMwwtqG0rH_O7QW7SRAoMg 
+https://github.com/gg-1414/music-visualizer*/
+/* Code Base from Gina Lee */
+
 window.addEventListener("DOMContentLoaded", init);
 function init() {
-  const canvas = document.querySelector("canvas");
+  //autoplay sound is not allowed on the web at the moment (2018/11/21)
+  document.querySelector("audio").addEventListener("click", initAudio);
+}
+
+function initAudio() {
+  //audio file
   const audio = document.querySelector("audio");
+
   //Canvas
+  const canvas = document.querySelector("canvas");
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   ctx = canvas.getContext("2d");
 
-  const context = new AudioContext();
+  //create audio Context only once
+  let context;
+  if (context == undefined) {
+    context = new AudioContext();
+  }
+  //connect source to the Analyser and the Analyser to AudioContext destination (audio output)
   let src = context.createMediaElementSource(audio);
   const analyser = context.createAnalyser();
-
   src.connect(analyser);
   analyser.connect(context.destination);
+
+  //lower the fft size for smaller amount of bars (but wider ones)
   analyser.fftSize = 16384;
+  //this is the audio data we are visualizing, frequency bin count
   const bufferLength = analyser.frequencyBinCount;
-  console.table(bufferLength);
+
   const dataArray = new Uint8Array(bufferLength);
-  console.log("DATA-ARRAY: ", dataArray);
+  //console.log("DATA-ARRAY: ", dataArray);
+
   const WIDTH = canvas.width;
   const HEIGHT = canvas.height;
 
@@ -27,7 +44,7 @@ function init() {
 
   let barHeight;
   let x = 0;
-  let bars = 150;
+  let bars = 360;
   audio.addEventListener("play", () => {
     function renderframe() {
       requestAnimationFrame(renderframe);
@@ -39,10 +56,16 @@ function init() {
 
       for (let i = 0; i < bars; i++) {
         barHeight = dataArray[i] * 2.5;
+        if (dataArray[i] > 200) {
+          ctx.fillStyle = "hsl(350, 50%, 50%)";
+        } else if (dataArray[i] > 180) {
+          ctx.fillStyle = "hsl(144, 50%, 50%)";
+        } else {
+          ctx.fillStyle = "hsl(256, 50%, 50%)";
+        }
 
-        ctx.fillStyle = "hsl(350, 50%, 50%)";
         ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
-
+        ctx.rotate(0.1);
         x += barWidth + 10;
       }
     }
